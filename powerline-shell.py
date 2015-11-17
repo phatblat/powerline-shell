b699836263ed5d394842e4959e3a198c3cd3418c
Ben Chatelain <ben@octop.ad>
2015-11-16 19:53:07 -0700

    Add exit_code segment to powerline prompt

--- powerline-shell.py
+++ powerline-shell.py
@@ -590,6 +590,16 @@ def add_jobs_segment():
 add_jobs_segment()
 
 
+def add_exit_code_segment():
+    if powerline.args.prev_error == 0:
+        return
+    fg = Color.CMD_FAILED_FG
+    bg = Color.CMD_FAILED_BG
+    powerline.append(str(powerline.args.prev_error), fg, bg)
+
+add_exit_code_segment()
+
+
 def add_root_indicator_segment():
     root_indicators = {
         'bash': ' \\$ ',
