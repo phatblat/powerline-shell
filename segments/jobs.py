46860fa80a795cb645253f219be488c62f0f8523
Ben Chatelain <ben@octop.ad>
2017-05-14 23:59:01 -0600

    Fix jobs count segment for Fish Shell
    banga/powerline-status#228

--- segments/jobs.py
+++ segments/jobs.py
@@ -2,10 +2,17 @@ import os
 import re
 import subprocess
 
+# Fixed job count for Fish Shell
+# https://github.com/banga/powerline-shell/issues/228
 def add_jobs_segment():
-    pppid = subprocess.Popen(['ps', '-p', str(os.getppid()), '-oppid='], stdout=subprocess.PIPE).communicate()[0].strip()
-    output = subprocess.Popen(['ps', '-a', '-o', 'ppid'], stdout=subprocess.PIPE).communicate()[0]
-    num_jobs = len(re.findall(str(pppid), output)) - 1
+    # pppid = subprocess.Popen(['ps', '-p', str(os.getppid()), '-o', 'ppid='], stdout=subprocess.PIPE).communicate()[0].strip()
+    pppid = os.getppid()
+
+    # -o ppid= removes the header row due to all headers being empty
+    output = subprocess.Popen(['ps', '-a', '-o', 'ppid='], stdout=subprocess.PIPE).communicate()[0]
+
+    # Why is this count 2 higher than expected?
+    num_jobs = len(re.findall(str(pppid), output)) - 2
 
     if num_jobs > 0:
         powerline.append(' %d ' % num_jobs, Color.JOBS_FG, Color.JOBS_BG)
